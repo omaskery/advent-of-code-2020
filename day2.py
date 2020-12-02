58e816cb6fd35e8007f6b912f9e6baafc4c0890c
Oliver Maskery <omaskery@googlemail.com>
2020-12-02 19:15:40 +0000

    Add comments to day 2 solution so far

--- day2.py
+++ day2.py
@@ -16,14 +16,18 @@ class PasswordEntry:
     password: str
 
 
+# this is a regular expression, it is designed to match specific patterns of text (the lines of text in the input file)
+# it splits the line up into the individual pieces described in the puzzle
 PASSWORD_ENTRY_REGEX = re.compile(r'^\s*(?P<min>\d+)-(?P<max>\d+)\s+(?P<letter>[a-zA-Z])\s*:\s*(?P<password>.+)\s*$')
 
 
 def build_password_entry_from_line(line: str) -> PasswordEntry:
+    # try and match our regular expression to the line of text from the input
     match = PASSWORD_ENTRY_REGEX.fullmatch(line)
     if not match:
         raise Exception(f"failed to parse password database entry: {line}")
 
+    # now it's matched, we can store the individual values into our password entry
     return PasswordEntry(
         policy=PasswordPolicy(
             min_occurrences=int(match.group('min')),
@@ -35,7 +39,9 @@ def build_password_entry_from_line(line: str) -> PasswordEntry:
 
 
 def does_password_satisfy_policy(password: str, policy: PasswordPolicy) -> bool:
+    # count the number of letters in the password that match the policy
     occurrences = len([letter for letter in password if letter == policy.letter])
+    # return whether the number of occurrences is within the limits specified in the policy
     return policy.min_occurrences <= occurrences <= policy.max_occurrences
 
 
@@ -44,6 +50,7 @@ def main():
     with open('input/day2.txt') as f:
         entries = [build_password_entry_from_line(line) for line in f if line]
 
+    # find all the entries that pass the corresponding password policy
     valid_entries = [entry for entry in entries if does_password_satisfy_policy(entry.password, entry.policy)]
 
     print(f"there seem to be {len(valid_entries)} entries!")
